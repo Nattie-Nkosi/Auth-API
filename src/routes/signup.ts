20a10c449e8d13dbba3e3da911adc4eb9d73529e
Nutty4616 <nkosin361@gmail.com>
2022-02-21 23:22:25 +0200

    User creation, Creating a user and Save data to MongoDb

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,7 +1,7 @@
 import expres, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connetion-error'; 
 
 const router = expres.Router();
 
@@ -22,10 +22,18 @@ router.post('/api/users/signup', [
 
   const { email, password } = req.body;
 
-  console.log('Creating a user...');
-  throw new DatabaseConnectionError();
+  const existingUser = await User.findOne({ email });
 
-  res.send({});
+  if (existingUser) {
+    console.log('Email in use');
+    res.send({});
+  }
+
+  const user = User.build({ email, password });
+  await user.save();
+
+  res.status(201).send(user)
+  
 });
 
 export { router as signupUserRouter };
