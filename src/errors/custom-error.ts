08371afaf05f1ab50cfe25cba77f8f271a47db98
Nutty4616 <nkosin361@gmail.com>
2022-02-21 12:36:20 +0200

    Route not found error

--- src/errors/custom-error.ts
+++ src/errors/custom-error.ts
@@ -1,7 +1,15 @@
-
 /**
  * @abstract List out all different properties that must be defined by any class that extends CustomError class.
  */
+
 export abstract class CustomError extends Error {
-  
+  abstract statusCode: number
+
+  constructor(message: string) {
+    super(message);
+
+    Object.setPrototypeOf(this, CustomError.prototype)
+  }
+
+  abstract serializeErrors(): { message: string, field?: string }[]
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import 'express-async-errors'
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
@@ -6,6 +7,7 @@ import { signinUserRouter } from './routes/signin';
 import { signupUserRouter } from './routes/signup';
 import { signoutUserRouter } from './routes/signout';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -14,6 +16,10 @@ app.use(currentUserRouter);
 app.use(signinUserRouter);
 app.use(signupUserRouter);
 app.use(signoutUserRouter);
+
+app.all('*', async (req: Request, res: Response) => {
+  throw new NotFoundError()
+})
 
 app.use(errorHandler);
 
