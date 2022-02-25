bc7d2ee5ec29500bda1380f332b3ccd57f22f7c5
Nutty4616 <nkosin361@gmail.com>
2022-02-25 20:07:04 +0200

    Getting started with cookie-session and jwt

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import 'express-async-errors'
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
 import dev from './config/dev';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinUserRouter } from './routes/signin';
@@ -12,7 +13,12 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(cookieSession({
+  signed: false,
+  secure: true
+}))
 
 app.use(currentUserRouter);
 app.use(signinUserRouter);
