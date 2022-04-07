8bc571da8d17689a401def86dbc56fd4f7f3002d
Nutty4616 <nkosin361@gmail.com>
2022-04-07 20:16:19 +0200

    signout and currentuser routes, requireAuth and currentUser Middleware

--- src/routes/signout.ts
+++ src/routes/signout.ts
@@ -3,7 +3,9 @@ import expres from 'express';
 const router = expres.Router();
 
 router.post('/api/users/signout', (req, res) => {
-  res.send('Hi There from routes folder')
+  req.session = null;
+
+  res.send({});
 });
 
 export { router as signoutUserRouter };
