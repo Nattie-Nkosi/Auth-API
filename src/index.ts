c1340d7809eb1073089beb59db9fdf9208c61f31
Nutty4616 <nkosin361@gmail.com>
2022-04-07 18:42:47 +0200

    Stopped tracking env.local, and env.staging

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ dotenv.config();
 
 const port = process.env.SERVER_PORT;
 const mongoUrl = process.env.MONGO_URL;
+const jwtKey = process.env.JWT_KEY;
 
 const app = express();
 app.set('trust proxy', true);
@@ -36,18 +37,18 @@ app.all('*', async (req: Request, res: Response) => {
 })
 
 app.use(errorHandler);
- 
+
 const start = async () => {
-  if(!process.env.JWT_KEY){
+  if (!jwtKey) {
     throw new Error('JWT_KEY must be defined')
   }
 
   try {
     await mongoose.connect(mongoUrl!);
     console.log('Connected to mongoDB')
-  } catch(err) {
+  } catch (err) {
     console.error(err)
-  } 
+  }
 
   app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
