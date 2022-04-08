83ec70fe2ba4a64a0793fb18d30e41554a910c0d
Nutty4616 <nkosin361@gmail.com>
2022-04-08 23:44:20 +0200

    Getting started with Unit Testing

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinUserRouter } from './routes/signin';
+import { signupUserRouter } from './routes/signup';
+import { signoutUserRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+dotenv.config();
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+  signed: false,
+  secure: true
+}))
+
+app.use(currentUserRouter);
+app.use(signinUserRouter);
+app.use(signupUserRouter);
+app.use(signoutUserRouter);
+
+app.all('*', async (req: Request, res: Response) => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler);
+
+export { app };
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,9 @@
-import dotenv from 'dotenv';
-import express, { Request, Response } from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinUserRouter } from './routes/signin';
-import { signupUserRouter } from './routes/signup';
-import { signoutUserRouter } from './routes/signout';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-// Initailize configuration
-dotenv.config();
+import { app } from './app';
 
 const port = process.env.SERVER_PORT;
 const mongoUrl = process.env.MONGO_URL;
 const jwtKey = process.env.JWT_KEY;
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(cookieSession({
-  signed: false,
-  secure: true
-}))
-
-app.use(currentUserRouter);
-app.use(signinUserRouter);
-app.use(signupUserRouter);
-app.use(signoutUserRouter);
-
-app.all('*', async (req: Request, res: Response) => {
-  throw new NotFoundError()
-})
-
-app.use(errorHandler);
 
 const start = async () => {
   if (!jwtKey) {
