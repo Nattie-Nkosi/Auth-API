c8e8d615f2eabaeb6147e5640a81fa7a2cc5fc9a
Nutty4616 <nkosin361@gmail.com>
2022-04-11 21:07:40 +0200

    Finishing signup tests

--- src/routes/__test__/signup.test.ts
+++ src/routes/__test__/signup.test.ts
@@ -46,3 +46,33 @@ it('returns a 400 with a missing email and password', async () => {
     })
     .expect(400); // 400 -> Bad Request
 });
+
+it('dissallows duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201)
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(400)
+});
+
+it('set a cookie after a successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
