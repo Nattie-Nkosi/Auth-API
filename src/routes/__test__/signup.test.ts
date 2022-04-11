f0934192e7b10453d44b28e99e8e02c0427e3605
Nutty4616 <nkosin361@gmail.com>
2022-04-11 20:39:09 +0200

    Testing invalid input

--- src/routes/__test__/signup.test.ts
+++ src/routes/__test__/signup.test.ts
@@ -8,5 +8,41 @@ it('returns a 201 on successful signup', async () => {
       email: 'test@test.com',
       password: 'password'
     })
-    .expect(201);
+    .expect(201);  // 201 -> Created
+});
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'asdfasdfasd',
+      password: 'password'
+    })
+    .expect(400); // 400 -> Bad Request
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'p'
+    })
+    .expect(400); // 400 -> Bad Request
+});
+
+it('returns a 400 with a missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+    })
+    .expect(400); // 400 -> Bad Request
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'asdfasdf'
+    })
+    .expect(400); // 400 -> Bad Request
 });
