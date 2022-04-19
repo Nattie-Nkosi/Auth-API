0ade1d177a00396dea66c831a618e0b047bb2b3a
Nutty4616 <nkosin361@gmail.com>
2022-04-19 21:45:21 +0200

    Setting up CORS

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
-
+import cors from 'cors';
 import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
@@ -15,12 +15,33 @@ import { NotFoundError } from './errors/not-found-error';
 dotenv.config();
 
 const app = express();
+
+// cors middleware
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token'
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: 'http://localhost:3000',
+  preflightContinue: false,
+};
+
+// https://auth-api-typescript.herokuapp.com/
+
+app.use(cors(options));
 app.set('trust proxy', true);
 app.use(json());
 app.use(cookieSession({
   signed: false,
   secure: process.env.NODE_ENV !== 'test'
 }));
+
+
 
 app.use(currentUserRouter);
 app.use(signinUserRouter);
@@ -29,7 +50,7 @@ app.use(signoutUserRouter);
 
 app.all('*', async (req: Request, res: Response) => {
   throw new NotFoundError()
-})
+});
 
 
 app.use(errorHandler);
